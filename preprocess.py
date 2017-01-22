a84f83c955b78769793618df89c022f658398967
无北 <conglaiwubei@163.com>
2017-01-21 17:50:32 -0800

    preprocess framework

---
+++ preprocess.py
@@ -0,0 +1,33 @@
+import nltk
+import csv
+
+def preprocess(filename):
+	def preprocessText(text):
+		def removeHTML(text):
+			return text
+		def removeURL(text):
+			return text
+		def removeAt(text):
+			return text
+		def removeHashTag(text):
+			return text
+
+		text = removeHTML(text)
+		text = removeURL(text)
+		text = removeAt(text)
+		text = removeHashTag(text)
+		return text
+
+	with open(filename, 'rb') as csvfile:
+		rows = csv.reader(csvfile, delimiter=',', quotechar='"')
+		line = 0
+		for row in rows:
+			line += 1
+			if line == 1:
+				continue
+			print preprocessText(row[10])
+			break
+
+
+if __name__ == '__main__':
+	preprocess('data/clinton-50k.csv')
